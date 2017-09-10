53ded1a70f8919fe23861ac827fc9df466717b98
Farghly Ahmed <eng_farghlyahmed@yahoo.com>
2017-09-10 18:58:54 +0200

    update functions file nav added

--- functions.php
+++ functions.php
@@ -55,6 +55,19 @@ if ( ! function_exists( 'loca_setup' ) ) :
             * add support change custom logo
         */
         add_theme_support( 'custom-logo' );
+
+        
+        // this function to add css and scripts from here
+/*
+
+function mytheme_style(){
+    wp_enqueue_style('style',get_template_directory_uri().'/assets/css/bootstrap.min.css');
+}
+add_action('wp_enqueue_scripts','mytheme_style');
+ */       
+        
+        
+/*custom logo setup*/
 function themename_custom_logo_setup() {
     $defaults = array(
         'height'      => 100,
@@ -73,10 +86,10 @@ if ( function_exists( 'the_custom_logo' ) ) {
 }
 
 		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
+register_nav_menus( array(
             'primary' => esc_html__( 'Primary', 'loca' )
 			
-		) );
+) );
         /**
          * add support to search form
          * 
@@ -326,6 +339,138 @@ function wplogin_filter( $url, $path, $orig_scheme )
  $new  = array( "b-login");
  return preg_replace( $old, $new, $url, 1);
 }*/
+
+
+/*
+  class of drop down menu
+*/
+    
+
+
+////////////////////////////////////////Copy class to functions///////////////////////////////////////////////////
+
+class BootstrapNavMenuWalker extends Walker_Nav_Menu {
+
+
+	function start_lvl( &$output, $depth ) {
+
+		$indent = str_repeat( "\t", $depth );
+		$submenu = ($depth > 0) ? ' sub-menu' : '';
+		$output	   .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+
+	}
+
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$li_attributes = '';
+		$class_names = $value = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		
+		// managing divider: add divider class to an element to get a divider before it.
+		$divider_class_position = array_search('divider', $classes);
+		if($divider_class_position !== false){
+			$output .= "<li class=\"divider\"></li>\n";
+			unset($classes[$divider_class_position]);
+		}
+		
+		$classes[] = ($args->has_children) ? 'dropdown' : '';
+		$classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
+		$classes[] = 'menu-item-' . $item->ID;
+		if($depth && $args->has_children){
+			$classes[] = 'dropdown-submenu';
+		}
+
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
+
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+		$attributes .= ($args->has_children) 	    ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
+
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= ($depth == 0 && $args->has_children) ? ' <b class="caret"></b></a>' : '</a>';
+		$item_output .= $args->after;
+
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+	
+
+	function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
+		//v($element);
+		if ( !$element )
+			return;
+
+		$id_field = $this->db_fields['id'];
+
+		//display this element
+		if ( is_array( $args[0] ) )
+			$args[0]['has_children'] = ! empty( $children_elements[$element->$id_field] );
+		else if ( is_object( $args[0] ) )
+			$args[0]->has_children = ! empty( $children_elements[$element->$id_field] );
+		$cb_args = array_merge( array(&$output, $element, $depth), $args);
+		call_user_func_array(array(&$this, 'start_el'), $cb_args);
+
+		$id = $element->$id_field;
+
+		// descend only when the depth is right and there are childrens for this element
+		if ( ($max_depth == 0 || $max_depth > $depth+1 ) && isset( $children_elements[$id]) ) {
+
+			foreach( $children_elements[ $id ] as $child ){
+
+				if ( !isset($newlevel) ) {
+					$newlevel = true;
+					//start the child delimiter
+					$cb_args = array_merge( array(&$output, $depth), $args);
+					call_user_func_array(array(&$this, 'start_lvl'), $cb_args);
+				}
+				$this->display_element( $child, $children_elements, $max_depth, $depth + 1, $args, $output );
+			}
+			unset( $children_elements[ $id ] );
+		}
+
+		if ( isset($newlevel) && $newlevel ){
+			//end the child delimiter
+			$cb_args = array_merge( array(&$output, $depth), $args);
+			call_user_func_array(array(&$this, 'end_lvl'), $cb_args);
+		}
+
+		//end this element
+		$cb_args = array_merge( array(&$output, $element, $depth), $args);
+		call_user_func_array(array(&$this, 'end_el'), $cb_args);
+
+	}
+
+}
+
+
+//////////////////////////Add This link to your menu///////////////////////////////
+
+// 'walker' => new BootstrapNavMenuWalker();
+
+
+
+
+
+
+
+/*
+    * end of class dropdown menu
+*/
 
 /**
  * Implement the Custom Header feature.
